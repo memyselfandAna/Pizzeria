0946a74bb09897e3318140779117512dee4ae41f
memyselfandAna <98179942+memyselfandAna@users.noreply.github.com>
2022-07-13 21:05:38 +0300

    add

---
+++ index.js
@@ -0,0 +1,23 @@
+// function initMap() {
+//     var location = {lat:45.43999, lng: 28.03918 };
+//     var map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 4,
+//         center:location
+//     });
+
+//     //var marker: location; 
+//     //Marker.setAnimation
+//     const marker = new google.maps.Marker({
+//         position: location,
+//         map: map
+//     });
+// }
+
+// window.initMap = initMap;
+
+const pizze = [
+    { nume: 'MARGHERITA', price: 20, img: 'Pizza la pachet.jpeg' }, 
+    { nume: 'Ulei Picant', price: 2, img: 'Ulei Picant.jpeg' }
+]
+
+console.log(pizze)
