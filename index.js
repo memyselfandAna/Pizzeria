3c14e51a26f49375e1b436020795301bbd5c3898
memyselfandAna <98179942+memyselfandAna@users.noreply.github.com>
2022-07-18 16:43:21 +0300

    Carusel BS

--- index.js
+++ index.js
@@ -1,23 +1,70 @@
-// function initMap() {
-//     var location = {lat:45.43999, lng: 28.03918 };
-//     var map = new google.maps.Map(document.getElementById("map"), {
-//         zoom: 4,
-//         center:location
-//     });
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} 
+else {
+    ready()
+}
 
-//     //var marker: location; 
-//     //Marker.setAnimation
-//     const marker = new google.maps.Marker({
-//         position: location,
-//         map: map
-//     });
-// }
+// const pizze = [
+   
+//     { nume: 'MARGHERITA', price: 20, img: 'Pizza la pachet.jpeg' },
+//     { nume: 'PROSCIUTTO E MAIS', price: 23, img: 'Pizza la pachet.jpeg' },
+//     { nume: 'PROSCIUTTO E FUNGHI', price: 23, img: 'Pizza la pachet.jpeg' },
+//     { nume: 'CAPRICIOSA', price: 24, img: 'Pizza la pachet.jpeg' },
+//     { nume: 'QUATRO STAGIONI', price: 25, img: 'Pizza la pachet.jpeg' },
+//     { nume: 'QUATRO FORMAGGI', price: 25, img: 'Pizza la pachet.jpeg' },
+//     { nume: 'NAPOLI', price: 25, img: 'Pizza la pachet.jpeg' },
+//     { nume: 'TONNO CIPOLA', price: 25, img: 'Pizza la pachet.jpeg' },
+//     { nume: 'TONNO OLIVE', price: 25, img: 'Pizza la pachet.jpeg' },
+//     { nume: 'DIAVOLA', price: 25, img: 'Pizza la pachet.jpeg' },
+//     { nume: 'FANTASIA', price: 26, img: 'Pizza la pachet.jpeg' },
+//     { nume: 'PRIMAVERA', price: 25, img: 'Pizza la pachet.jpeg' },
+//     { nume: 'VEGETARIANA', price: 25, img: 'Pizza la pachet.jpeg' }, 
+//     { nume: 'MARIA', price: 25, img: 'Pizza la pachet.jpeg'},
+//     { nume: 'DELIZIA', price: 26, img: 'Pizza la pachet.jpeg'},
+//     { nume: 'RUSTICA', price: 25, img: 'Pizza la pachet.jpeg' }
+// ]
 
-// window.initMap = initMap;
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
 
-const pizze = [
-    { nume: 'MARGHERITA', price: 20, img: 'Pizza la pachet.jpeg' }, 
-    { nume: 'Ulei Picant', price: 2, img: 'Ulei Picant.jpeg' }
-]
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('input', quantityChanged)
+    }
+}
 
-console.log(pizze)
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.previousElementSibling.value = 0;
+    updateCartTotal()
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value < 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var marcel = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < marcel.length; i++) {
+        var cartRow = marcel[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace(' Lei', ''))
+        var quantity = parseInt(quantityElement.value)
+        total = total + (price * quantity)
+    }
+    total = (Math.round(total * 100) / 100).toFixed(2)
+    document.getElementsByClassName('cart-total-price')[0].innerText = total + ' Lei';
+}
